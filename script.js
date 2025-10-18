c09bd947503866de4de2830db41166bb6233e20b
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2025-10-18 01:17:49 +0000

    [Optimizing Code]

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//step 1 Array for generate excuses
+// 1 Array por variable. 
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-//step 2 randomness fuction 
-function random(arr){
-    return arr[Math.floor(Math.random()* arr.length)];
-}
-
-//complete excuse function
-function generateExcuse() {
-    let randomWho = random(who);
-    let randomAction = random(action);
-    let randomWhat = random(what);
-    let randomWhen = random(when);
-    
-    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
-}
-
-// Onload
+function random(exc){
+   return exc[Math.floor(Math.random()* exc.length)]
+};
+ 
 window.onload = function() {
-    let excuseElement = document.getElementById("excuse");
-    excuseElement.innerHTML = generateExcuse();
+  //write your code here
+  let randomWho = random(who);
+  let randomAction= random(action);
+  let randomWhat = random(what);
+  let randomWhen = random(when);
+
+  document.getElementById("excuse").innerText = randomWho + " "  + randomAction + " " + randomWhat + " " + randomWhen;
 };
 
